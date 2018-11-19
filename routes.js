f3f6870d484f36d84fa6f0cc4773761a08582ec8
Alunos <Alunos>
2018-11-19 19:14:50 -0300

    Backup

--- routes.js
+++ routes.js
@@ -5,6 +5,9 @@ const routes = express.Router();
 const UserController = require('./src/controllers/UserController');
 
 routes.post('/registro', UserController.store);
+routes.post('/recuperar/senha/:email', UserController.updatePassword);
+
+routes.get('/enviar/email/:email/:password', UserController.retrivePassword);
 routes.get('/users/email/:email', UserController.getUserByEmailRegister);
 routes.get('/users/username/:username', UserController.getUserByUsernameRegister);
 routes.get('/admin/:username', UserController.getUserByUsernameRegister);
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,55 +1,110 @@
 const Users = require('../models/Users');
+const nodemailer = require('nodemailer');
 
 module.exports = {
-    async index(req, res) {
-        const users = await Users.find({}).sort("-createdAt");
+	async index(req, res) {
+		const users = await Users.find({}).sort("-createdAt");
 
-        return res.json(users);
-    },
+		return res.json(users);
+	},
 
-    async store(req, res) {
-        try {
-            const users = await Users.create(req.body);
+	async store(req, res) {
+		try {
+			const users = await Users.create(req.body);
 			users.save();
-            return res.json(users);
-        } catch (error) {
-            return console.error(error);
-        }
-        //req.io.emit('users', users);
-    
-    },
+			return res.json(users);
+		} catch (error) {
+			return console.error(error);
+		}
+		//req.io.emit('users', users);
 
-    async getUserByEmailRegister(req, res) {
-			let email = req.params.email;
-            const users = await Users.findOne({
-				email: email
-			}).then(data => {
-				return res.status(200).send(data);
-			}).catch(err => {
-				return res.status(400).send('E-mail ou usuário em uso!');
-			});
-    },
-	
+	},
+
+	async getUserByEmailRegister(req, res) {
+		let email = req.params.email;
+		const users = await Users.findOne({
+			email: email
+		}).then(data => {
+			return res.status(200).send(data);
+		}).catch(err => {
+			return res.status(400).send('E-mail ou usuário em uso!');
+		});
+	},
+
 	async getUserByUsernameRegister(req, res) {
-			let username = req.params.username;
-            const users = await Users.findOne({
-				username: username
-			}).then(data => {
-				return res.status(200).send(data);
-			}).catch(err => {
-				return res.status(400).send('E-mail ou usuário em uso!');
-			});
-    },
-	
+		let username = req.params.username;
+		const users = await Users.findOne({
+			username: username
+		}).then(data => {
+			return res.status(200).send(data);
+		}).catch(err => {
+			return res.status(400).send('E-mail ou usuário em uso!');
+		});
+	},
+
 	async getUserAdmin(req, res) {
-			let username = req.params.username;
-            const users = await Users.findOne({
-				admin: true,
-				username: username
-			}).then(data => {
-				return res.status(200).send(data);
-			}).catch(err => {
-				return res.status(400).send('Usuário não possui acesso de administrador!');
+		let username = req.params.username;
+		const users = await Users.findOne({
+			admin: true,
+			username: username
+		}).then(data => {
+			return res.status(200).send(data);
+		}).catch(err => {
+			return res.status(400).send('Usuário não possui acesso de administrador!');
+		});
+	},
+
+	async updatePassword(req, res) {
+		let newPass = "";
+		for (let index = 0; index <= 10; index++) {
+			let random = Math.floor((Math.random() * 10));
+			newPass += random;
+		}
+		let email = req.params.email;
+		const users = await Users.findOneAndUpdate(
+			{
+				email: email
+			},
+			{
+				$set: {
+					password: newPass
+				}
 			});
-    }
+
+		const password = await Users.findOne({ email: email });
+
+		return res.status(200).send(password);
+	},
+
+	async retrivePassword(req, res) {
+		let $destinatario = req.params.email;
+		let $password = req.params.password;
+
+		let $usuario = 'suportebetting@gmail.com';
+		let $senha = 'supbetting123';
+		
+		let transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: $usuario,
+				pass: $senha
+			}
+		});
+
+		let mailOptions = {
+			from: $usuario,
+			to: $destinatario,
+			subject: 'Recuperação de senha',
+			text: `Sua nova senha: ${$password}`
+		};
+
+		transporter.sendMail(mailOptions, function (error, info) {
+			if (error) {
+				console.log(error);
+			} else {
+				res.status(200).send('Email enviado: ' + info.response);
+				return console.log('Email enviado: ' + info.response);
+			}
+		});
+	}
 }
