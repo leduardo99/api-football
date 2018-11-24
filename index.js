39527db922adab6c050b4b0969eb3659482e0202
leduardo99 <luiseduardo@computex.com.br>
2018-11-24 17:30:38 -0300

    Correção do index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
 const server = require('http').Server(app);
-//const io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
-const port = process.env.PORT || 15000;
+const port = process.env.PORT || 54987;
 
-mongoose.connect('mongodb://leduardo:913700lL@ds115244.mlab.com:15244/footbetting', { useNewUrlParser: true });
+mongoose.connect('mongodb://leduardo:913700lL>mongodb://<dbuser>:<dbpassword>@ds115244.mlab.com:15244/footbetting', { useNewUrlParser: true });
 
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+})
+
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
