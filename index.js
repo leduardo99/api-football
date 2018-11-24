42dc9d1dcb07356059010b076616385fb419476b
leduardo99 <luiseduardo@computex.com.br>
2018-11-24 18:05:52 -0300

    Correção do index.js

--- index.js
+++ index.js
@@ -17,10 +17,17 @@ app.use((req, res, next) => {
     return next();
 })
 
-app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+//app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
 server.listen(port, () => {
-    console.log('Servidor iniciado com sucesso na porta '+port);
+    console.log('Servidor iniciado com sucesso na porta ' + port);
 });
